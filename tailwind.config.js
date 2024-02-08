802a43225312d50e0fb6918d48f2a8b75ccc1179
zeeunltd <azegaf@gmail.com>
2024-02-08 23:47:43 +0100

    Added dynamic sidebar links

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,40 +2,39 @@
 module.exports = {
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     colors: {
-      'blue': '#1fb6ff',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-      // 
-      'primary': '#9999BC',
-
+      blue: "#1fb6ff",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+      primary: "#9999BC",
+      robin: "#EA8D51",
     },
     backgroundColor: {
-      'main-bg': '#F3F7FF'
+      "main-bg": "#F3F7FF",
     },
     fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-      title: ['Hanken Grotesk']
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+      title: ["Hanken Grotesk"],
     },
     extend: {
       spacing: {
-        '10.5': '42rem',
-        '128': '32rem',
-        '144': '36rem',
+        10.5: "42rem",
+        128: "32rem",
+        144: "36rem",
       },
       borderRadius: {
-        '4xl': '2rem',
-      }
-    }
-  }
+        "4xl": "2rem",
+      },
+    },
+  },
 }
